2f942b8198f7a689a407feec04a8090272cd2630
Axel Guzmán <axl_bg@hotmail.com>
2025-01-17 20:04:44 -0300

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Permite solicitudes desde el frontend
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
+  // Configuración de puerto
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
