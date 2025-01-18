768523b804d04a1940619aa73d922b69beaf944e
Axel Guzmán <axl_bg@hotmail.com>
2025-01-18 19:57:38 -0300

    add filter by category

--- frontend/src/app/components/note-list/note-list.component.ts
+++ frontend/src/app/components/note-list/note-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Note, NoteService } from '../../services/note.service';
 import { CommonModule, NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Category, CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-note-list',
@@ -18,12 +19,23 @@ export class NoteListComponent {
   archivedSelected: boolean = false;
   term: string = '';
 
-  constructor(private noteService: NoteService) {
+  defaultMessageFilter: string = 'Filter by category...';
+  categoryFilter: string = this.defaultMessageFilter;
+  categories: Category[] = [];
+
+  constructor(
+    private noteService: NoteService,
+    private categoryService: CategoryService
+  ) {
     this.noteService.notes$.subscribe((data) => {
       this.notes = data;
       this.filteredNotes = this.notes;
     });
     this.fetchNotes();
+
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
   }
 
   outNote(note: Note) {
@@ -33,27 +45,44 @@ export class NoteListComponent {
   fetchNotes(): void {
     this.noteService.getActiveNotes().subscribe((data) => {
       this.notes = data;
-      this.filteredNotes = this.notes;
+      this.filteredNotes = this.filterNotesByCategory(this.notes);
     });
   }
 
   fetchArchivedNotes(): void {
     this.noteService.getArchivedNotes().subscribe((data) => {
       this.notes = data;
-      this.filteredNotes = this.notes;
+      this.filteredNotes = this.filterNotesByCategory(this.notes);
     });
   }
 
   filterNotes() {
     const searchTerm = this.term.toLowerCase().trim();
     if (searchTerm) {
-      console.log('log');
-      this.filteredNotes = this.notes.filter(
+      let staticNotes = this.filterNotesByCategory(this.notes);
+      this.filteredNotes = staticNotes.filter(
         (note: any) =>
           note.title && note.title.toLowerCase().includes(searchTerm)
       );
     } else {
-      this.filteredNotes = this.notes;
+      this.filteredNotes = this.filterNotesByCategory(this.notes);
     }
+  }
+
+  filterByCategory() {
+    this.filteredNotes = this.filterNotesByCategory(this.notes);
+  }
+
+  filterNotesByCategory(notes: Note[]): Note[] {
+    const searchTerm = this.categoryFilter;
+    if (searchTerm == this.defaultMessageFilter) {
+      return notes;
+    }
+
+    let filteredNotes: Note[];
+    filteredNotes = notes.filter(
+      (note: any) => note.category.name == searchTerm
+    );
+    return filteredNotes;
   }
 }
